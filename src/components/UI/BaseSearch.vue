facadc91733f2fd345f6720b9c0bd51f39831bbe
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2024-10-27 07:57:08 +0100

    initial commit

---
+++ src/components/UI/BaseSearch.vue
@@ -0,0 +1,34 @@
+<template>
+  <form>
+    <input type="search" @input="search" :value="searchTerm" placeholder="Filter items" />
+  </form>
+</template>
+
+<script setup>
+import {defineProps, defineEmits} from 'vue';
+
+  const props = defineProps({
+    searchTerm: String
+  });
+  const emit = defineEmits('search');
+  const search = (event) => {
+    console.log(props)
+    emit('search', event.target.value)
+  }
+</script>
+
+<style scoped>
+input {
+  font: inherit;
+  width: 100%;
+  display: block;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus {
+  outline: none;
+  border-color: #00006b;
+  background-color: #eeeeff;
+}
+</style>
